47b1cf677d0df513477cd87d8bdb76ba119127d8
Krohit22 <rajpurohitkrish45666@gmail.com>
2025-12-15 22:56:13 +0530

    port problem

--- main.py
+++ main.py
@@ -48,4 +48,4 @@ async def predict(file: UploadFile = File(...)):
 if __name__ == "__main__":
     # Use the PORT environment variable provided by Render
     port = int(os.getenv("PORT", 8000))
-    uvicorn.run(app, host="0.0.0.0", port=port)
+    uvicorn.run(app, host="0.0.0.0", port=8000)
