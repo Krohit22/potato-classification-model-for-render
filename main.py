f9da36b68d4e12115f681dab1bf441d70f8cab3f
Krohit22 <rajpurohitkrish45666@gmail.com>
2025-12-16 08:30:37 +0530

    rewrite

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-import tensorflow as tf
 from fastapi import FastAPI, UploadFile, File
+import tensorflow as tf
 from fastapi.middleware.cors import CORSMiddleware
 import uvicorn
 import numpy as np
@@ -9,43 +8,29 @@ from PIL import Image
 
 app = FastAPI()
 
-origins = [
-    "http://localhost",
-    "http://localhost:3000",  # This is for your React app running locally
-]
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=origins,
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
 
 Model = tf.keras.models.load_model("./potatoes.h5")
 CLASS_NAMES = ["Early Blight", "Late Blight", "Healthy"]
 
-@app.get("/ping")
-async def ping():
-    return "Hello, I am alive!"
+@app.get('/')
+def read_root():
+    return {'message': 'potatos classification model API'}
 
 def read_file_as_image(data) -> np.ndarray:
     image = np.array(Image.open(BytesIO(data)))
     return image
 
 @app.post("/predict")
-async def predict(file: UploadFile = File(...)):
+async def predict(file: UploadFile=File(...)):
     image = read_file_as_image(await file.read())
-    image_batch = np.expand_dims(image, 0)
+    image_batch = np.expand_dims(image,0)
     predictions = Model.predict(image_batch)
     prediction_class = CLASS_NAMES[np.argmax(predictions[0])]
     confidence = np.max(predictions[0])
     print(prediction_class, confidence)
-    return {
-        "class": prediction_class,
-        "confidence": float(confidence)
+    return{
+        "class" : prediction_class,
+        "confidence" : float(confidence)
     }
 
-if __name__ == "__main__":
-    # Use the PORT environment variable provided by Render
-    port = int(os.getenv("PORT", 8000))
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    
